f62c626f2cb7d30fe21adb260bfa3cf04a294cfe
User <wer@qer.3>
2021-10-05 12:02:12 +0300

    added main

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
+import asyncio
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Socket:
+    def __init__(self):
+        self.socket = socket.socket(
 
+            socket.AF_INET,
+            socket.SOCK_STREAM,
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.main_loop = asyncio.new_event_loop()
+
+    async def send_data(self, data=None):
+        raise NotImplementedError()
+
+    async def listen_socket(self, listened_socket=None):
+        raise NotImplementedError()
+
+    async def main(self):
+        raise NotImplementedError()
+
+    def start(self):
+        self.main_loop.run_until_complete(self.main())
+
+    def set_up(self):
+        raise NotImplementedError()
+
